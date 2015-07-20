65e0b5b59971c6a4f9150b9cbaaa97db0777db66
Colm Morgan <cmorgan@globalpersonals.co.uk>
2015-07-20 13:02:03 +0100

    Add routes for Leagues and Seasons

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,14 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
-  resources :teams
+  resources :leagues
+  resources :seasons
   resources :players do
     # Show all fixtures for a specified Player
     # resources :fixtures
 
     # Show all teams played by a specified Player
-  
+
     # Show all leagues including a specified Player
     # resources :leagues
 
