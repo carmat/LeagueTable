18037b617bcb343c78a6181bc1a0fe793131de34
Colm Morgan <cmorgan@globalpersonals.co.uk>
2015-07-27 14:13:51 +0100

    Add 'remember_digest' back after schema refactoring

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150726213555) do
+ActiveRecord::Schema.define(version: 20150727130951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20150726213555) do
     t.string   "team"
     t.integer  "league_id"
     t.boolean  "admin",           default: false
+    t.string   "remember_digest"
   end
 
   add_index "players", ["email"], name: "index_players_on_email", unique: true, using: :btree
