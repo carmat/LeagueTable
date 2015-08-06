d1685e843206502ce15936207e335581299e081d
Colm Morgan <cmorgan@globalpersonals.co.uk>
2015-08-06 23:05:37 +0100

    Add Admin to Seasons

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729171819) do
+ActiveRecord::Schema.define(version: 20150806213629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,9 +70,13 @@ ActiveRecord::Schema.define(version: 20150729171819) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "admin"
   end
+
+  add_index "seasons", ["admin"], name: "index_seasons_on_admin", using: :btree
 
   add_foreign_key "fixtures", "leagues"
   add_foreign_key "leagues", "seasons"
   add_foreign_key "players", "leagues"
+  add_foreign_key "seasons", "players", column: "admin"
 end
